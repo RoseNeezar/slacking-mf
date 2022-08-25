f0b2cd952f623bb8cfff0989f2372c613602b195
Neezar <rforlyffe@gmaicl.com>
2022-08-25 23:57:42 +0800

    add input controller

--- src/controllers/inputManager.ts
+++ src/controllers/inputManager.ts
@@ -1,1 +1,46 @@
-export default class InputManager {}
+import { KeyboardEvent } from "react";
+import { KeyboardEventKey } from "../types/keyboardType";
+
+export default class InputManager {
+  observers: Function[] = [];
+
+  subscribe(fn: Function) {
+    this.observers.push(fn);
+  }
+
+  unsubscribe(fn: Function) {
+    this.observers = this.observers.filter((sub) => sub != fn);
+  }
+
+  broadcast(action: string, data: unknown) {
+    this.observers.forEach((sub) => sub(action, data));
+  }
+
+  handleKeys = (ev: KeyboardEvent | Event) => {
+    ev.preventDefault();
+    switch ((ev as KeyboardEvent).key as KeyboardEventKey) {
+      case "ArrowLeft":
+        this.broadcast("move", { x: -1, y: 0 });
+        break;
+      case "ArrowUp":
+        this.broadcast("move", { x: 0, y: 1 });
+        break;
+      case "ArrowRight":
+        this.broadcast("move", { x: 1, y: 0 });
+        break;
+      case "ArrowDown":
+        this.broadcast("move", { x: 0, y: -1 });
+        break;
+      default:
+        break;
+    }
+  };
+
+  bindKeys() {
+    document.addEventListener("keydown", this.handleKeys);
+  }
+
+  unbindKeys() {
+    document.removeEventListener("keydown", this.handleKeys);
+  }
+}

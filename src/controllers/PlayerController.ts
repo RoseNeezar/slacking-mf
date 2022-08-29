d71a43f6869c20de3b6ac96f6b0feb6027985057
Neezar <rforlyffe@gmaicl.com>
2022-08-29 15:41:48 +0800

    add image as player

--- src/controllers/PlayerController.ts
+++ src/controllers/PlayerController.ts
@@ -8,11 +8,22 @@ export default class Player {
   x: number;
   y: number;
   size: number;
+  color: string;
+  image = new Image();
+  width = 82;
+  height = 82;
+  frameX = 0;
+  frameY = 0;
+
+  moving = false;
 
   constructor({ x = 0, y = 0, size = 10 }: Partial<IPlayer> = {}) {
     this.x = x;
     this.y = y;
     this.size = size;
+
+    this.color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    this.image.src = "img/feral.png";
   }
 
   move(dx: number, dy: number) {
@@ -21,10 +32,47 @@ export default class Player {
   }
 
   draw(context: CanvasRenderingContext2D) {
-    context.fillStyle = "#f00";
-    context.textBaseline = "hanging";
-    context.font = "16px";
-    // steps can take
-    context.fillText("@", this.x * this.size, this.y * this.size);
+    context.imageSmoothingEnabled = false;
+    if (!this.image.complete) {
+      this.image.onload = () => {
+        context.drawImage(
+          this.image,
+          this.width,
+          this.height * this.frameY,
+          this.width,
+          this.height,
+          this.x * this.size,
+          this.y * this.size,
+          this.size,
+          this.size
+        );
+      };
+    } else {
+      context.drawImage(
+        this.image,
+        0,
+        this.height * this.frameY,
+        this.width,
+        this.height,
+        this.x * this.size,
+        this.y * this.size,
+        this.size,
+        this.size
+      );
+    }
+  }
+
+  handlePlayerFrame() {
+    if (this.frameX < 3) {
+      this.frameX++;
+    } else {
+      this.frameX = 0;
+    }
+  }
+
+  copyPlayer() {
+    const newPlayer = new Player();
+    Object.assign(newPlayer, this);
+    return newPlayer;
   }
 }
--- src/controllers/inputManager.ts
+++ src/controllers/inputManager.ts
@@ -1,8 +1,14 @@
 import { KeyboardEvent } from "react";
 import { KeyboardEventKey } from "../types/keyboardType";
+import Player from "./PlayerController";
 
 export default class InputManager {
   observers: Function[] = [];
+
+  playerEntity: Player;
+  constructor(player: Player) {
+    this.playerEntity = player;
+  }
 
   subscribe(fn: Function) {
     this.observers.push(fn);
@@ -22,15 +28,19 @@ export default class InputManager {
     switch ((ev as KeyboardEvent).key as KeyboardEventKey) {
       case "a":
         this.broadcast("move", { x: -1, y: 0 });
+        this.playerEntity.frameY = 1;
         break;
       case "w":
         this.broadcast("move", { x: 0, y: -1 });
+        // this.playerEntity.frameY = 3;
         break;
       case "d":
         this.broadcast("move", { x: 1, y: 0 });
+        this.playerEntity.frameY = 0;
         break;
       case "s":
         this.broadcast("move", { x: 0, y: 1 });
+        // this.playerEntity.frameY = 0;
         break;
       default:
         break;
